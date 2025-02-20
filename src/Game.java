faca185d62df174abeaaf7b8bec7a1f4bb732b6a
Saksakun Jiarapong <117532774+Vacoom12@users.noreply.github.com>
2025-02-21 02:50:57 +0700

    create game structure

---
+++ src/Game.java
@@ -0,0 +1,80 @@
+package src;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Dimension;
+import java.awt.Color;
+
+public class Game extends JPanel implements Runnable {
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale;
+    public final int maxScreenCol = 28;
+    public final int maxScreenRow = 16;
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+    public final int maxWorldCol = maxScreenCol;
+    public final int maxWorldRow = maxScreenRow;
+
+    Thread gameThread;
+    int FPS = 60;
+
+    public Game() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        // this.addKeyListener();
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        // int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                // update();
+                // repaint();
+                delta--;
+                // drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                // drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+        
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        // tileM.draw(g2);
+
+        g2.dispose();
+    }
+}
