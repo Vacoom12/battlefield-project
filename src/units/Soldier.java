4048acd20538b390f5e03afcabf2867c426a6435
Saksakun Jiarapong <117532774+Vacoom12@users.noreply.github.com>
2025-02-23 03:29:52 +0700

    add some units

---
+++ src/units/Soldier.java
@@ -0,0 +1,7 @@
+package src.units;
+
+public class Soldier extends Unit {
+    public Soldier (String name, int x, int y, int width, int height, int health) {
+        super("Soldier", x, y, width, height, health);
+    }
+}
---
+++ src/units/Unit.java
@@ -0,0 +1,25 @@
+package src.units;
+
+public class Unit {
+    protected int x, y;    
+    protected int width, height; 
+    protected int health;      
+    protected String name;  
+
+    public Unit(String name, int x, int y, int width, int height, int health) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.health = health;
+    }
+
+    public void takeDamage() {
+        if (health > 0) health--;
+    }
+
+    public boolean isDestroyed() {
+        return health == 0;
+    }
+}
